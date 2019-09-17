44b0faf8ea06e04e96910f30285340769b5ee701
Ligia09 <ligia_babos@yahoo.com>
2019-09-18 00:42:41 +0300

    master - original :master

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Ligia");
+
+      //Tipuri de date
+
+
+        System.out.println("sum" );
+        int x = 6;
+        int y = 7;
+        int sum = (x+y);
+        float average =sum/2;
+        System.out.println(average);
+
+        byte t = -5;
+        byte u = 8;
+        byte i = 6;
+        byte sum;
+        sum = (-t+u);
+        System.out.println("Numbers " -t + u );
+        float average =sum*i;
+        System.out.println(average);
+
+
+
+
+
+
+
+    }
+}
